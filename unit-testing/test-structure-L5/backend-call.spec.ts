514b8a4bb2d69574f445e145134f458ce5ed87fa
Nico Vogel <nico-vogel97@outlook.de>
2022-05-02 12:29:28 +0200

    Add level 5

---
+++ unit-testing/test-structure-L5/backend-call.spec.ts
@@ -0,0 +1,40 @@
+import { expect, use } from 'chai';
+import sinon from 'ts-sinon'
+import * as sinonChai from 'sinon-chai';
+import { backendCall } from './backend-call';
+use(sinonChai);
+
+describe('L5 - ', () => {
+  let clock: sinon.SinonFakeTimers;
+  beforeEach(() => {
+    // mock the browser internal timer
+    clock = sinon.useFakeTimers();
+  })
+
+  afterEach(() => sinon.restore());
+
+  it('should handle backend call', async () => {
+    // Given
+
+    // When
+    const result = backendCall(42);
+
+    // Then
+    // tick lets time pass
+    clock.tick(50);
+    const value = await result;
+    expect(value).to.equal(50);
+  })
+
+  it('should handle backend call - done function', async (done) => {
+    // Given
+
+    // When
+    // actually waits for 50 ms for the test
+    const value = await backendCall(42);
+
+    // Then
+    expect(value).to.equal(50);
+    done();
+  })
+});
---
+++ unit-testing/test-structure-L5/backend-call.ts
@@ -0,0 +1,4 @@
+
+export async function backendCall<T>(input: T) {
+  return new Promise<T>(resolve => setTimeout(() => resolve(input), 50));
+}
